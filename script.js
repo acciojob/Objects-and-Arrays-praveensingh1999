2c722b5cbcc00b3415f8964e820aad0cc092d7a7
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-12-18 21:02:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const person = {
   age: 80,
 };
 
-// Step 1: Reference to the players array
+// Reference to the same array
 const team = players;
 
-// Step 2: Copy of the players array
-const team1 = [...players]; // or players.slice()
+// Copy of the array
+const team1 = [...players];
 
-// Step 3: Copy of the person object
-const cap1 = { ...person }; // or Object.assign({}, person);
+// Copy of the object
+const cap1 = { ...person };
